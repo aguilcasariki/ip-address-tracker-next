31460ac7ad572e37d9547da0f8213b21fbd9a3b9
Riki <aguilcasariki@gmail.com>
2024-01-07 22:50:38 -0500

    Update frontend structure

---
+++ app/(client)/app.js
@@ -0,0 +1,86 @@
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import IpInput from "./components/IpInput/IpInput";
+import Map from "./components/Map/Map";
+import fetchIpGeoData from "./api/fetchIpGeoData";
+import { useCallback, useMemo, useState } from "react";
+import IpCard from "./components/IpCard/IpCard";
+import useTimezoneConvert from "./hooks/useTimezoneConvert";
+const loadingMsg = "Loading...";
+
+const App = () => {
+  // Define the initial state of inputValue and ip as empty strings.
+  const [inputValue, setInputValue] = useState("");
+  const [ip, setIp] = useState("");
+
+  // Use the useQuery hook to fetch the IP geolocation data.
+  const { isLoading, data, isError, refetch } = useQuery({
+    queryKey: ["geoData", ip], // Unique key to identify the query.
+    queryFn: () => fetchIpGeoData(ip), // Function that returns the query promise.
+    refetchOnWindowFocus: false, // Disable automatic refetch on window focus.
+  });
+
+  // Define the callbacks to handle form change and submission.
+  const handleChange = useCallback((event) => {
+    setInputValue(event.target.value);
+  }, []);
+
+  const handleSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      setIp(inputValue.trim());
+      refetch(); // Re-fetch the IP geolocation data.
+    },
+    [inputValue, refetch]
+  );
+
+  // Use a custom hook to convert the timezone to a readable string.
+  const timezones = useTimezoneConvert(data?.timezone);
+
+  // Use useMemo to avoid unnecessary calculations of center and cardData.
+  const center = useMemo(
+    () => (data ? [data.lat, data.lon] : [34.0648, -118.086]),
+    [data]
+  );
+
+  const cardData = useMemo(
+    () => [
+      {
+        title: "IP ADDRESS",
+        info: data?.query ?? loadingMsg,
+      },
+      {
+        title: "LOCATION",
+        info: data
+          ? `${data.city}, ${data.region} 
+        ${data.zip}`
+          : loadingMsg,
+      },
+      {
+        title: "TIMEZONE",
+        info: timezones
+          ? `${timezones[0]}
+        ${timezones[1]}`
+          : loadingMsg,
+      },
+      {
+        title: "ISP",
+        info: data?.isp ?? loadingMsg,
+      },
+    ],
+    [data, timezones]
+  );
+
+  // Render the IpInput, IpCard, and Map components with the fetched information.
+  return (
+    <div className="h-screen flex flex-col items-center">
+      <IpInput handleChange={handleChange} handleSubmit={handleSubmit} />
+      {isLoading && <p>{loadingMsg}</p>}
+      {isError && <p>Error: {isError.message}</p>}
+      <IpCard cardData={cardData} />
+      <Map position={center} />
+    </div>
+  );
+};
+
+export default App;
--- app/(client)/page.js
+++ app/(client)/page.js
@@ -1,93 +1,15 @@
-import React from "react";
+"use client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import App from "./app";
+// Create a client
+const queryClient = new QueryClient();
 
-function page() {
-  return <div>page</div>;
+function Page() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
 }
 
-export default page;
-
-/* import { useQuery } from "@tanstack/react-query";
-import IpInput from "../components/IpInput/IpInput";
-import Map from "../components/Map/Map";
-import fetchIpGeoData from "../api/fetchIpGeoData";
-import { useCallback, useMemo, useState } from "react";
-import IpCard from "../components/IpCard/IpCard";
-import useTimezoneConvert from "../hooks/useTimezoneConvert";
-
-const App = () => {
-  // Define the initial state of inputValue and ip as empty strings.
-  const [inputValue, setInputValue] = useState("");
-  const [ip, setIp] = useState("");
-
-  // Use the useQuery hook to fetch the IP geolocation data.
-  const { isLoading, data, isError, refetch } = useQuery({
-    queryKey: ["geoData", ip], // Unique key to identify the query.
-    queryFn: () => fetchIpGeoData(ip), // Function that returns the query promise.
-    refetchOnWindowFocus: false, // Disable automatic refetch on window focus.
-  });
-
-  // Define the callbacks to handle form change and submission.
-  const handleChange = useCallback((event) => {
-    setInputValue(event.target.value);
-  }, []);
-
-  const handleSubmit = useCallback(
-    (event) => {
-      event.preventDefault();
-      setIp(inputValue.trim());
-      refetch(); // Re-fetch the IP geolocation data.
-    },
-    [inputValue, refetch]
-  );
-
-  // Use a custom hook to convert the timezone to a readable string.
-  const timezones = useTimezoneConvert(data?.timezone);
-
-  // Use useMemo to avoid unnecessary calculations of center and cardData.
-  const center = useMemo(
-    () => (data ? [data.lat, data.lon] : [34.0648, -118.086]),
-    [data]
-  );
-
-  const cardData = useMemo(
-    () => [
-      {
-        title: "IP ADDRESS",
-        info: data?.query ?? "Loading...",
-      },
-      {
-        title: "LOCATION",
-        info: data
-          //? `${data.city}, ${data.region} 
-        ${data.zip}`
-          : "Loading...",
-      },
-      {
-        title: "TIMEZONE",
-        info: timezones
-          // ? `${timezones[0]}
-        ${timezones[1]}`
-          : "Loading...",
-      },
-      {
-        title: "ISP",
-        info: data?.isp ?? "Loading...",
-      },
-    ],
-    [data, timezones]
-  );
-
-  // Render the IpInput, IpCard, and Map components with the fetched information.
-  return (
-    <div className="h-screen flex flex-col items-center">
-      <IpInput handleChange={handleChange} handleSubmit={handleSubmit} />
-      {isLoading && <p>Loading...</p>}
-      {isError && <p>Error: {isError.message}</p>}
-      <IpCard cardData={cardData} />
-      <Map position={center} />
-    </div>
-  );
-};
-
-export default App;
- */
+export default Page;
