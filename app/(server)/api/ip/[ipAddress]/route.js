b25c6f2f63336f1c8b1098b756ac4c85c0d3e6f3
Riki <aguilcasariki@gmail.com>
2024-01-09 00:07:13 -0500

    Delete debug console logs in API code

--- app/(server)/api/ip/[ipAddress]/route.js
+++ app/(server)/api/ip/[ipAddress]/route.js
@@ -2,24 +2,22 @@ import { NextResponse } from "next/server";
 import Geo from "../../../(models)/GeoIp";
 
 export async function GET(request, { params }) {
-  console.log("Now in the get");
-
   try {
-    if (!params.ipAddress) {
-      throw new Error("IP address or domain is missing");
-    }
-
     const geo = await Geo.findOne({
       $or: [{ ip: params.ipAddress }, { domain: params.ipAddress }],
     });
-
     return geo
       ? NextResponse.json(geo)
-      : NextResponse.json({ error: "GeoIp data not found." }, { status: 404 });
+      : NextResponse.json(
+          { error: "GeoIp data not found for this IP or domain." },
+          { status: 404 }
+        );
   } catch (error) {
-    console.error(error);
     return NextResponse.json(
-      { error: "An error occurred while searching for the GeoIp data." },
+      {
+        error:
+          "An error occurred while searching for the GeoIp data for this IP or domain.",
+      },
       { status: 500 }
     );
   }
