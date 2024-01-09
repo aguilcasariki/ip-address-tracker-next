b5bc552db2c6cb69b331a3f8dbd7f1c07ee34529
Riki <aguilcasariki@gmail.com>
2024-01-09 00:02:20 -0500

    Update API

--- app/(server)/api/ip/[ipAddress]/route.js
+++ app/(server)/api/ip/[ipAddress]/route.js
@@ -2,19 +2,24 @@ import { NextResponse } from "next/server";
 import Geo from "../../../(models)/GeoIp";
 
 export async function GET(request, { params }) {
-  console.log("Ya este en el get");
+  console.log("Now in the get");
 
   try {
-    const geo = await Geo.findOne({ ip: params.ipAddress });
+    if (!params.ipAddress) {
+      throw new Error("IP address or domain is missing");
+    }
+
+    const geo = await Geo.findOne({
+      $or: [{ ip: params.ipAddress }, { domain: params.ipAddress }],
+    });
+
     return geo
       ? NextResponse.json(geo)
-      : NextResponse.json(
-          { error: "No se encontró el GeoIp." },
-          { status: 404 }
-        );
+      : NextResponse.json({ error: "GeoIp data not found." }, { status: 404 });
   } catch (error) {
+    console.error(error);
     return NextResponse.json(
-      { error: "Hubo un error al buscar el GeoIp." },
+      { error: "An error occurred while searching for the GeoIp data." },
       { status: 500 }
     );
   }
