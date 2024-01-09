c015a9e361de1776a1741126dfa84ac319aa3f66
Riki <aguilcasariki@gmail.com>
2024-01-08 22:46:42 -0500

    Deploy API on Vercel

--- app/(server)/api/ip/[ipAddress]/route.js
+++ app/(server)/api/ip/[ipAddress]/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import Geo from "../../../(models)/GeoIp";
 
 export async function GET(request, { params }) {
-  console.log("ya este en el get");
+  console.log("Ya este en el get");
 
   try {
     const geo = await Geo.findOne({ ip: params.ipAddress });
